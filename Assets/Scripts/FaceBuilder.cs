61c8d0f617a49ba8f762dccf5c8094096507bd50
Alexander Jeremy <wierd_al_ex@hotmail.com>
2018-04-22 17:39:39 +1000

    Resource fetch script
    Made a simple script that accepts a list of face element names and fetches the sprites for them. Renamed the resource paths so the same name could be used for the category displayed in the interface.

---
+++ Assets/Scripts/FaceBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceBuilder : MonoBehaviour {
+	public string[] elements;
+	public Dictionary<string, Sprite[]> elementOptions;
+
+	void Start()
+	{
+		foreach(string e in elements)
+		{
+			elementOptions.Add(e, Array.ConvertAll(Resources.LoadAll(e + "/", typeof(Sprite)), x => (Sprite)x));
+		}
+	}
+
+	public Sprite getRandomElementSprite(string elementName)
+	{
+		return elementOptions[elementName][UnityEngine.Random.Range(0, elementOptions[elementName].Length - 1)];
+	}
+}
